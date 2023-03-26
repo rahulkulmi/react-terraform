52d1c12623a543effaa67cb0e59473e182aebc9b
rahulkulmi <rahul@newput.com>
2023-03-26 12:56:12 +0530

    for s3 website we need cert inside us east.

--- infra/data.tf
+++ infra/data.tf
@@ -1,15 +1,15 @@
-# provider "aws" {
-#   alias      = "virginia"
-#   region     = "us-east-1"
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-# }
+provider "aws" {
+  alias      = "virginia"
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
 
 data "aws_acm_certificate" "ssl_cert" {
   domain      = var.certificate_name
   most_recent = true
   statuses    = ["ISSUED"]
-  # provider    = aws.virginia
+  provider    = aws.virginia
 }
 
 data "aws_route53_zone" "main" {
